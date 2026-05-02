69ef6d1f4e319d9d5438e4a588ed017ba2fc0b01
Ayush Goel <ayushgoel544@gmail.com>
2026-05-02 11:23:50 +0530

    Fix formatting in Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { assets } from '../assets/assets'
-
+ 
 const Footer = () => {
   return (
     <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
